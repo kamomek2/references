a674fb6808853631e498801b7f20a9f1bc6289ad
kamomek2 <z.kanie@gmail.com>
2024-07-15 13:21:53 +0900

    add

---
+++ packages/connectors/src/index.ts
@@ -0,0 +1,9 @@
+export { Connector } from './base'
+export type { ConnectorData, ConnectorEvents } from './base'
+
+export {
+  ChainNotConfiguredForConnectorError,
+  ConnectorNotFoundError,
+} from './errors'
+
+export type { WindowProvider } from './types'
